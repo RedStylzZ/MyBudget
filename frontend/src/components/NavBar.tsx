033e8a21a58ee0eac7d6ffa6a2c42553713f7aa9
Tizian <tizianappler@yahoo.de>
2022-01-31 11:09:58 +0100

    Added buttons to NavBar and fixed NavBar to the left

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,9 +1,13 @@
 import './NavBar.scss'
+import {Link} from "react-router-dom";
 
 export default function NavBar() {
     return (
         <div className={"navBar"}>
-
+            <Link to={"/"}>
+                <input type={"button"} value={"Home"}/>
+            </Link>
+            <input type={"button"} value={"Login"}/>
         </div>
     )
 }
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,19 @@
+import Categories from "../components/Categories";
+import Recent from "../components/Recent";
+import './HomePage.scss'
 
 export default function HomePage() {
+
+
+
     return (
         <div className={"homePage"}>
-            
+            <div className={"recentOuts"}>
+                <Recent />
+            </div>
+            <div className={"homeCategories"}>
+                <Categories />
+            </div>
         </div>
     )
 }
