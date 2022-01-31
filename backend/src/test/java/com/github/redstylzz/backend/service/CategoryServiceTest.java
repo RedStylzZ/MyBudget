2c1c244a915e483b39984cc76e040771a8df2951
Tizian <tizianappler@yahoo.de>
2022-01-31 19:12:31 +0100

    Add CategoryServiceTest

---
+++ backend/src/test/java/com/github/redstylzz/backend/service/CategoryServiceTest.java
@@ -0,0 +1,60 @@
+package com.github.redstylzz.backend.service;
+
+import com.github.redstylzz.backend.exception.CategoryAlreadyExistException;
+import com.github.redstylzz.backend.model.Category;
+import com.github.redstylzz.backend.model.TestDataProvider;
+import com.github.redstylzz.backend.repository.ICategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+class CategoryServiceTest {
+
+    private final ICategoryRepository repository = mock(ICategoryRepository.class);
+    private final CategoryService underTest = new CategoryService(repository);
+
+    @BeforeEach
+    void beforeEach() {
+        when(repository.findAllByUserID(anyString())).thenReturn(new ArrayList<>());
+    }
+
+    @Test
+    void getCategories() {
+        underTest.getCategories(TestDataProvider.testUser());
+
+        verify(repository).findAllByUserID(anyString());
+    }
+
+    @Test
+    void shouldThrowExceptionIfCategoryExists() {
+        when(repository.findAllByUserID(anyString()))
+                .thenReturn(List.of(TestDataProvider.testCategory()));
+
+        assertThrows(CategoryAlreadyExistException.class, () -> {
+            underTest.addCategory(TestDataProvider.testUser(), TestDataProvider.testCategory().getName());
+        });
+    }
+
+    @Test
+    void shouldAddCategoryIfNotExistentAndReturnCategories() {
+        when(repository.save(any(Category.class))).thenReturn(null);
+
+        underTest.addCategory(TestDataProvider.testUser(), TestDataProvider.testCategory().getName());
+
+        verify(repository).save(any(Category.class));
+    }
+
+    @Test
+    void renameCategory() {
+    }
+
+    @Test
+    void deleteCategory() {
+    }
+}
