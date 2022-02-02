8bd9ae4e6b83812d4b5eab823a0e46e00976b7ac
Tizian <tizianappler@yahoo.de>
2022-02-02 15:45:38 +0100

    Added Test for user fetch from principal

--- backend/src/test/java/com/github/redstylzz/backend/service/MongoUserServiceTest.java
+++ backend/src/test/java/com/github/redstylzz/backend/service/MongoUserServiceTest.java
@@ -6,7 +6,10 @@ import com.github.redstylzz.backend.repository.IMongoUserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.security.Principal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -31,5 +34,22 @@ class MongoUserServiceTest {
                 .thenReturn(user);
 
         assertEquals(user, underTest.loadUserByUsername(""));
+    }
+
+    @Test
+    void shouldThrowExceptionIfPrincipalIsNull() {
+        assertThrows(UsernameNotFoundException.class, () -> underTest.getUserByPrincipal(null));
+    }
+
+    @Test
+    void shouldReturnUser() {
+        Principal principal = mock(Principal.class);
+        MongoUser actualUser = TestDataProvider.testUser();
+        when(principal.getName()).thenReturn(actualUser.getUsername());
+        when(repository.findMongoUserByUsername(anyString())).thenReturn(actualUser);
+
+        MongoUser user = underTest.getUserByPrincipal(principal);
+
+        assertEquals(actualUser, user);
     }
 }
