c02935e2acb582026159c8fc6905eff56ef6582a
Tizian <tizianappler@yahoo.de>
2022-02-04 11:25:02 +0100

    Disabled scrollen on site, added scrolling for categories and payments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                {/*<NavBar/>*/}
+                <NavBar/>
                 <AuthProvider>
                     <Routes>
                         <Route path={"/login"} element={<LoginPage/>}/>
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,16 +1,24 @@
 import Categories from "../components/Categories";
 import Recent from "../components/Recent";
 import './HomePage.scss'
-import {useContext, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import {AuthContext} from "../context/AuthProvider";
-import {IPaymentController} from "../models/ControllerTypes";
+import {ICategoryController, IPaymentController} from "../models/ControllerTypes";
 import PaymentController from "../controllers/PaymentController";
+import CategoryController from "../controllers/CategoryController";
+import {Category} from "../models/Category";
 
 export default function HomePage() {
     const token: string = useContext(AuthContext).token!
+    const config = {headers: {Authorization: `Bearer ${token}`}}
     const paymentController: IPaymentController = PaymentController()
+    const categoryController: ICategoryController = CategoryController(config)
     const [payments, setPayments] = useState()
+    const [categories, setCategories] = useState<Category[]>([])
 
+    useEffect(() => {
+        categoryController.getCategories().then(setCategories)
+    }, [])
 
     return (
         <div className={"homePage"}>
@@ -18,7 +26,8 @@ export default function HomePage() {
                 <Recent />
             </div>
             <div className={"homeCategories"}>
-                {/*<Categories />*/}
+                <h1>Categories</h1>
+                <Categories categories={categories}/>
             </div>
         </div>
     )
