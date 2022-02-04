64d034afaa004ea6d5b7b375689669a46e7f0993
Tizian <tizianappler@yahoo.de>
2022-02-04 14:43:01 +0100

    CSS fix

--- frontend/src/pages/CategoryPage.tsx
+++ frontend/src/pages/CategoryPage.tsx
@@ -1,9 +1,13 @@
 import Categories from "../components/Categories";
-import {useContext, useEffect, useState} from "react";
+import {FormEventHandler, useContext, useEffect, useState} from "react";
 import {ICategoryController} from "../models/ControllerTypes";
 import CategoryController from "../controllers/CategoryController";
 import {Category} from "../models/Category";
 import {AuthContext} from "../context/AuthProvider";
+
+interface ITextInput {
+    categoryInput: { value: string }
+}
 
 export default function CategoryPage() {
     const token: string = useContext(AuthContext).token!
@@ -15,10 +19,27 @@ export default function CategoryPage() {
         controller.getCategories().then(setCategories)
     }, [])
 
+    const addCategory: FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault()
+        const form = event.currentTarget
+        const formElements = form.elements as typeof form.elements & ITextInput
+        const category = formElements.categoryInput.value.trim()
+
+        if (category && category.length > 0) {
+            controller.getCategories().then(setCategories)
+        }
+    }
+
     return (
         <div className={"categoryPage"}>
-            <h1>Categories</h1>
+            <div className={"categoryHeader"}>
+                <h1>Categories</h1>
+            </div>
             <div className={"categories"}>
+                <form onSubmit={addCategory} className={"addCategory"}>
+                    <input type="text" id="categoryInput"/>
+                    <input type="submit" value={"Add category"}/>
+                </form>
                 <Categories categories={categories}/>
             </div>
         </div>
