4adc20c6bd3a6ec3e1b37d6d04011a58241a28e5
Tizian <tizianappler@yahoo.de>
2022-02-07 15:27:58 +0100

    Removed console.log

--- frontend/src/controllers/LoginController.ts
+++ frontend/src/controllers/LoginController.ts
@@ -15,9 +15,6 @@ export default function LoginController(): ILoginController {
         checkLoggedIn: () => {
             const dateInMilliseconds: number = jwtDecoded.exp! * 1000
             if (isNaN(dateInMilliseconds)) return true
-            console.log(jwtDecoded)
-            console.log("Token time: " + dateInMilliseconds)
-            console.log("Current time: " + Date.now())
             return dateInMilliseconds > Date.now()
         }
     }
